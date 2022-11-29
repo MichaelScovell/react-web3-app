6937a242375ae8867e872032d1f283213a53946b
MichaelScovell <michael.scovell@cevo.com.au>
2022-11-29 16:26:46 +1100

    Added Styling to background and font

--- full-mint-website/src/App.js
+++ full-mint-website/src/App.js
@@ -5,10 +5,15 @@ import NavBar from './components/NavBar'
 
 function App() {
   const [accounts, setAccounts] = useState([]);
+
+
   return (
-    <div className="App">
-      <NavBar accounts={accounts} setAccounts={setAccounts} />
-      <MainMint accounts={accounts} setAccounts={setAccounts} />
+    <div className='overlay'>
+      <div className="App">
+        <NavBar accounts={accounts} setAccounts={setAccounts} />
+        <MainMint accounts={accounts} setAccounts={setAccounts} />
+      </div>
+      <div className='moving-background'></div>
     </div>
   );
 }
